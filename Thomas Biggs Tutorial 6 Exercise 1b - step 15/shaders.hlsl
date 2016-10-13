b646aa007d3a69c3e09c81caaeb9ffff892ede21
BiggT93 <0biggt83@solent.ac.uk>
2016-10-13 13:53:33 +0100

    Add files via upload

---
+++ Thomas Biggs Tutorial 6 Exercise 1b - step 15/shaders.hlsl
@@ -0,0 +1,31 @@
+struct VOut
+{
+	float4 position : SV_POSITION;
+	float4 color	: COLOR;
+};
+
+cbuffer CBuffer0
+{
+	matrix WVPMatrix;	 // 64bytes
+	float scale;		 //4 bytes
+	float red_fraction;  // 4 bytes
+	float3 packing;		 // 3x4 bytes
+	
+};
+VOut VShader(float4 position : POSITION, float4 color : COLOR)
+{
+	VOut output;
+
+	color.r *= red_fraction;
+	output.position = mul(WVPMatrix, position);
+	
+	//output.position.x -= scale;
+	output.color = color;
+
+	return output;
+}
+
+float4 PShader(float4 position : SV_POSITION, float4 color : COLOR) : SV_TARGET
+{
+	return color;
+}
